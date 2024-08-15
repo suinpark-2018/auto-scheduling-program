b6be819a069adc0f6efe6754370a81aa4e75f054
suinpark <suinpark@gmail.com>
2024-08-16 00:38:19 +0900

    Feat: 회원가입 관련 데이터 유효성 검증 기능 구현
    - 회원가입 시 입력된 데이터의 유효성 검증을 위한 Validation 클래스(ValidationGroups, ValidationSequence) 구현 완료

---
+++ src/main/java/com/schedule/common/validation/ValidationGroups.java
@@ -0,0 +1,9 @@
+package com.schedule.common.validation;
+
+public class ValidationGroups {
+    public interface NotBlankGroup {}
+    public interface PatternGroup {}
+    public interface SizeGroup {}
+    public interface IdCheckGroup {}
+//    public interface RegisterGroup {}
+}
