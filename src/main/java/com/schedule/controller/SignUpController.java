a0a169315fe2a5792a5b0d8e678a0da360c27a36
suinpark <suinpark@gmail.com>
2024-08-18 17:49:44 +0900

    Feat: 본인 인증 메일 기능 위한 Controller 구현
    - 회원가입 전 본인 인증 메일 전송 기능을 위한 SignUpController 구현 완료

--- src/main/java/com/schedule/controller/SignUpController.java
+++ src/main/java/com/schedule/controller/SignUpController.java
@@ -1,8 +1,10 @@
 package com.schedule.controller;
 
+import ch.qos.logback.classic.pattern.RelativeTimeConverter;
 import com.schedule.common.validation.*;
 import com.schedule.dto.StaffDto;
 import com.schedule.service.StaffServiceImpl;
+import com.schedule.service.mail.MailService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -12,6 +14,8 @@ import org.springframework.validation.annotation.Validated;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -21,6 +25,64 @@ import java.util.Map;
 public class SignUpController {
     private final StaffServiceImpl staffService;
     private BCryptPasswordEncoder passwordEncoder;
+    private final MailService mailService;
+
+    @GetMapping("/verification")
+    public String verification() {
+        return "identityVerification";
+    }
+
+    @PostMapping("/verificationEmail")
+    @ResponseBody
+    public ResponseEntity<String> verificationEmail(@RequestBody @Validated(ValidationGroups.EmailCheckGroup.class) StaffDto staffDto, HttpServletRequest request) {
+        // 사용자가 입력한 이메일
+        String email = staffDto.getEmail();
+
+        // 세션에 저장된 인증번호와 새로 생성한 인증번호 확인
+        HttpSession session = request.getSession();
+        String savedMailKey = (String) session.getAttribute("mailKey");
+        String newMailKey = mailService.makeRandomMailKey();
+        // 최종적으로 정해진 인증번호를 메일로 전송
+        String mailKey = staffService.makeVerificationCode(savedMailKey, newMailKey);
+
+        try {
+            if (staffService.sendVerificationEmail(email, mailKey)) {
+                session.setAttribute("mailKey", mailKey); // 세션에 인증번호 저장
+                session.setMaxInactiveInterval(60); // 세션 만료 시간 설정(1분)
+                return new ResponseEntity<>("인증번호 전송 성공", HttpStatus.OK);
+            } else {
+                return new ResponseEntity<>("해당 이메일은 이미 사용 중입니다.", HttpStatus.BAD_REQUEST);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            e.getMessage();
+            return new ResponseEntity<>("인증번호 전송에 실패했습니다.", HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    // 인증번호 일치여부 확인
+    @PostMapping("/verificationCode")
+    @ResponseBody
+    public ResponseEntity<String> verificationCode(@RequestBody Map<String, String> requestBody, HttpServletRequest request) {
+        // 세션에 저장해둔 인증번호(savedMailKey)
+        HttpSession session = request.getSession();
+        String savedMailKey = (String) session.getAttribute("mailKey");
+
+        // 사용자가 입력한 인증번호(inputMailKey)
+        String inputMailKey = requestBody.get("inputMailKey");
+
+        try {
+            if (inputMailKey.equals(savedMailKey)) {
+                return new ResponseEntity<>("인증번호 전송 성공", HttpStatus.OK);
+            } else {
+                return new ResponseEntity<>("인증번호가 일치하지 않습니다.", HttpStatus.BAD_REQUEST);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            e.getMessage();
+            return new ResponseEntity<>("인증번호 전송에 실패했습니다.", HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
 
     @GetMapping("/form")
     public String moveToRegisterPage() {
@@ -81,4 +143,5 @@ public class SignUpController {
             }
         }
     }
+
 }
